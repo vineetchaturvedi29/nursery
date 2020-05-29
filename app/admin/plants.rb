2f9695b76141f588683b8e07bc4e459866e881da
Vineet <chaturvedivineet2910@gmail.com>
2020-05-29 15:33:58 +0530

    Update plant.rb

--- app/admin/plants.rb
+++ app/admin/plants.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Plant do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :price, :avatar_file_name, :avatar_content_type, :avatar_file_size, :avatar_updated_at, :detail, :category_id
+  permit_params :name, :price,:detail, :category_id, :avatar #:avatar_file_name, :avatar_content_type, :avatar_file_size, :avatar_updated_at, :detail, :category_id
   #
   # or
   #
@@ -16,14 +16,24 @@ ActiveAdmin.register Plant do
   # end
 
   form do |f|
-      f.inputs do
-        f.input :category, as: :select
-        f.input :name
-        f.input :price
-        f.input :detail
-        f.input :avatar, as: :file, label: 'Image'
-      end
-      f.actions
+    f.inputs do
+      f.input :category, as: :select
+      f.input :name
+      f.input :price
+      f.input :detail
+      f.input :avatar, as: :file, label: 'Image'
     end
+    f.actions
+  end
   
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :price
+    column :avatar
+    column :detail
+    column :category
+    actions
+  end
 end
